2749f377e324ba8e52282f507dc6925989d13019
jose22gonzalez <97250949+jose22gonzalez@users.noreply.github.com>
2022-03-31 20:27:24 -0400

    Proyecto Final

--- Entidades/Inscripciones.cs
+++ Entidades/Inscripciones.cs
@@ -3,8 +3,10 @@ namespace Jose_Gonzalez_Ap1_PF.Entidades
     public class Inscripciones
     {
         public int InscripcionId { get; set; }
-        public DateTime Fecha { get; set; }
+        public DateTime? Fecha { get; set; } = null;
         public int EstudianteId { get; set; }
+
+        public virtual List<InscripcionesDetalles> InscripcionesDetalles {get; set;} = new List<InscripcionesDetalles>();
         
     }
 }
